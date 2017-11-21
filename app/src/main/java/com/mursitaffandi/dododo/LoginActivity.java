50ddacc8e27bf822efab109f0f7882fc6081fb66
Aditya Widayanto <adityawi@devjurnal.com>
2017-11-21 23:41:26 +0700

    loginjava

--- app/src/main/java/com/mursitaffandi/dododo/LoginActivity.java
+++ app/src/main/java/com/mursitaffandi/dododo/LoginActivity.java
@@ -2,12 +2,37 @@ package com.mursitaffandi.dododo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
+    EditText edt_email, edt_password;
+    Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView() {
+        edt_email = (EditText) findViewById(R.id.edt_email);
+        edt_password = (EditText) findViewById(R.id.edt_password);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btn_login :
+                String email = edt_email.getText().toString();
+                String password = edt_password.getText().toString();
+                break;
+        }
     }
 }
