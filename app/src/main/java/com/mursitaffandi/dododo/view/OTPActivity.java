bc32967d4246d2cc329608ec6b28f604c6031b8c
Aditya Widayanto <adityawi@devjurnal.com>
2017-11-23 10:26:18 +0700

    last

--- app/src/main/java/com/mursitaffandi/dododo/view/OTPActivity.java
+++ app/src/main/java/com/mursitaffandi/dododo/view/OTPActivity.java
@@ -13,15 +13,30 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.mursitaffandi.dododo.R;
+import com.mursitaffandi.dododo.util.ConfigUrl;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OTPActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -45,18 +60,55 @@ public class OTPActivity extends AppCompatActivity implements View.OnClickListen
     }
 public void countDown(){
     btn_resend_code.setVisibility(View.INVISIBLE);
-    CountDownTimer Count = new CountDownTimer(60000, 1000) {
-        public void onTick(long millisUntilFinished) {
-            tv_timer.setText(+ millisUntilFinished / 1000 +"detik lagi");
-        }
-
-        public void onFinish() {
-            tv_timer.setVisibility(View.INVISIBLE);
-            btn_resend_code.setVisibility(View.VISIBLE);
-        }
-    };
-    Count.start();
+    send_numberOTP();
 }
+
+    private void send_numberOTP() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ConfigUrl.BASE_URL + "send", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    Log.d("dfdf",response);
+                    JSONArray jsonArr = new JSONArray(response);
+                    JSONObject jsonOb = jsonArr.getJSONObject(0);
+                    boolean isSuccec = jsonOb.getBoolean("sukses");
+                    if (isSuccec){
+                        CountDownTimer Count = new CountDownTimer(60000, 1000) {
+                            public void onTick(long millisUntilFinished) {
+                                tv_timer.setText(+ millisUntilFinished / 1000 +"detik lagi");
+                            }
+
+                            public void onFinish() {
+                                tv_timer.setVisibility(View.INVISIBLE);
+                                btn_resend_code.setVisibility(View.VISIBLE);
+                            }
+                        };
+                        Count.start();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> param = new HashMap<>();
+                param.put("no_hp", getIntent().getStringExtra("no_hp"));
+                return param;
+            }
+        };
+        requestQueue.add(stringRequest);
+        Log.d("links",ConfigUrl.BASE_URL+"send");
+        Log.d("no_hp",getIntent().getStringExtra("no_hp"));
+    }
+
     private void initView() {
         edt_otp1 = (EditText) findViewById(R.id.edt_otp1);
         tv_timer = (TextView) findViewById(R.id.tv_timer);
