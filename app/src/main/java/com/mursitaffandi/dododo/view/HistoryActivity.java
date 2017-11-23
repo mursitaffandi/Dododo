05bf5fedaa92f96e07164d452da4c05bd96c25b2
Aditya Widayanto <adityawi@devjurnal.com>
2017-11-23 10:40:35 +0700

    historylayout

--- app/src/main/java/com/mursitaffandi/dododo/view/HistoryActivity.java
+++ app/src/main/java/com/mursitaffandi/dododo/view/HistoryActivity.java
@@ -1,7 +1,9 @@
-package com.mursitaffandi.dododo;
+package com.mursitaffandi.dododo.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mursitaffandi.dododo.R;
 
 public class HistoryActivity extends AppCompatActivity {
 
